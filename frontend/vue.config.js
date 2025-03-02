8422d2155ae1def0f522962276c22831b5315169
An <tqa24@duck.com>
2025-03-02 21:05:46 +0700

    Changed name of application to Chat App

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,14 @@
-// frontend/vue.config.js
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/static/', //  Tell Vue CLI to use /static/ as the base
-})
+  publicPath: '/static/', // Tell Vue CLI to use /static/ as the base
+  productionSourceMap: false, // Disable source maps in production
+
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Chat App';
+      return args;
+    });
+  }
+});
